bc13ea346b6a2f7409661049bb3f6bb6e87bde6a
mainfranc <igoromskiy@gmail.com>
2021-05-14 16:23:16 +0300

    Initial commit

---
+++ Find_File.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Find_File.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(601, 475)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_3)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label = QLabel(self.frame_3)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout_3.addWidget(self.label)
+
+        self.txtFileName = QLineEdit(self.frame_3)
+        self.txtFileName.setObjectName(u"txtFileName")
+
+        self.horizontalLayout_3.addWidget(self.txtFileName)
+
+        self.chkFileNameReg = QCheckBox(self.frame_3)
+        self.chkFileNameReg.setObjectName(u"chkFileNameReg")
+
+        self.horizontalLayout_3.addWidget(self.chkFileNameReg)
+
+
+        self.verticalLayout_2.addWidget(self.frame_3)
+
+        self.frame_4 = QFrame(self.frame)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_2 = QLabel(self.frame_4)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_2.addWidget(self.label_2)
+
+        self.txtTextToFind = QLineEdit(self.frame_4)
+        self.txtTextToFind.setObjectName(u"txtTextToFind")
+
+        self.horizontalLayout_2.addWidget(self.txtTextToFind)
+
+        self.chkTextToFindReg = QCheckBox(self.frame_4)
+        self.chkTextToFindReg.setObjectName(u"chkTextToFindReg")
+
+        self.horizontalLayout_2.addWidget(self.chkTextToFindReg)
+
+
+        self.verticalLayout_2.addWidget(self.frame_4)
+
+        self.frame_5 = QFrame(self.frame)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame_5)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_3 = QLabel(self.frame_5)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        self.txtDirectory = QLineEdit(self.frame_5)
+        self.txtDirectory.setObjectName(u"txtDirectory")
+
+        self.horizontalLayout.addWidget(self.txtDirectory)
+
+        self.btnBrowse = QPushButton(self.frame_5)
+        self.btnBrowse.setObjectName(u"btnBrowse")
+
+        self.horizontalLayout.addWidget(self.btnBrowse)
+
+
+        self.verticalLayout_2.addWidget(self.frame_5)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+        self.frame_2 = QFrame(self.centralwidget)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.tblFiles = QTableWidget(self.frame_2)
+        if (self.tblFiles.columnCount() < 2):
+            self.tblFiles.setColumnCount(2)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tblFiles.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tblFiles.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        self.tblFiles.setObjectName(u"tblFiles")
+        self.tblFiles.setEnabled(True)
+
+        self.horizontalLayout_4.addWidget(self.tblFiles)
+
+
+        self.verticalLayout.addWidget(self.frame_2)
+
+        self.frame_6 = QFrame(self.centralwidget)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.lblStatus = QLabel(self.frame_6)
+        self.lblStatus.setObjectName(u"lblStatus")
+
+        self.horizontalLayout_5.addWidget(self.lblStatus)
+
+        self.horizontalSpacer = QSpacerItem(426, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_5.addItem(self.horizontalSpacer)
+
+        self.btnFindFile = QPushButton(self.frame_6)
+        self.btnFindFile.setObjectName(u"btnFindFile")
+
+        self.horizontalLayout_5.addWidget(self.btnFindFile)
+
+
+        self.verticalLayout.addWidget(self.frame_6)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 601, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Find File", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"File name", None))
+        self.chkFileNameReg.setText(QCoreApplication.translate("MainWindow", u"Use Regex", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Text to find", None))
+        self.chkTextToFindReg.setText(QCoreApplication.translate("MainWindow", u"Use Regex", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Directory", None))
+        self.btnBrowse.setText(QCoreApplication.translate("MainWindow", u"Browse folder", None))
+        ___qtablewidgetitem = self.tblFiles.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"File name", None));
+        ___qtablewidgetitem1 = self.tblFiles.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Size", None));
+        self.lblStatus.setText(QCoreApplication.translate("MainWindow", u"", None))
+        self.btnFindFile.setText(QCoreApplication.translate("MainWindow", u"Find", None))
+    # retranslateUi
+
---
+++ Main.py
@@ -0,0 +1,102 @@
+from PySide2.QtCore import (Signal, QThread, Qt, QSettings, QFile, QIODevice, QTextStream)
+from PySide2.QtGui import QCloseEvent, QDesktopServices
+from PySide2.QtWidgets import QMainWindow, QWidget, QApplication, QDialog, QTableWidgetItem, QFileDialog
+
+import time
+import re
+import os, sys
+
+import Find_File
+
+class Monitor(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.ui = Find_File.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.regex_filename = False
+        self.regex_text = False
+        self.match_count = 0
+
+        #Buttons click
+        self.ui.btnBrowse.clicked.connect(self.btnBrowsePressed)
+        self.ui.btnFindFile.clicked.connect(self.btnFindFileclicked)
+
+    def btnBrowsePressed(self):
+        folder_name = QFileDialog.getExistingDirectory()
+        if not os.path.isfile(folder_name):
+            self.ui.txtDirectory.setText(folder_name)
+            return None
+        print('Please choose the correct folder')
+
+    def btnFindFileclicked(self):
+        self.match_count = 0
+        self.result = []
+        self.list_folder_structure(self.ui.txtDirectory.text())
+        self.update_table()
+
+    def list_folder_structure(self, start_dir):
+        os.chdir(start_dir)
+        curr_folder = start_dir
+        for i in os.listdir(path='.'):
+            is_it_file = os.path.isfile(curr_folder + '/' + i)
+            if is_it_file:
+                if self.check_is_right(i, curr_folder):
+                    self.result.append((i, curr_folder))
+            else:
+                self.list_folder_structure(curr_folder + '/' + i)
+
+    def update_table(self):
+        self.ui.tblFiles.clearContents()
+        for row in range(self.ui.tblFiles.rowCount()):
+            self.ui.tblFiles.removeRow(row)
+        rowPosition = 0
+        for i in self.result:
+            self.ui.tblFiles.insertRow(rowPosition)
+            self.ui.tblFiles.setItem(rowPosition, 0, QTableWidgetItem(i[1] + "/" + i[0]))
+            rowPosition += 1
+        self.ui.lblStatus.setText(f"{self.match_count} file{'s' if self.match_count else ''} found")
+
+    def check_is_right(self, filename, filepath):
+        self.regex_filename = self.ui.chkFileNameReg.isChecked()
+        self.regex_text = self.ui.chkTextToFindReg.isChecked()
+        return all((self.file_name_appl(filename), self.text_in_file(filename, filepath)))
+
+    def file_name_appl(self, filename):
+        fn_to_search = self.ui.txtFileName.text()
+        if self.regex_filename:
+            does_match = re.compile(fn_to_search).match
+            if does_match(filename):
+                return True
+            return False
+        if fn_to_search in filename:
+            return True
+        return False
+
+    def text_in_file(self, filename, filepath):
+        filepath = filepath if filepath[-1] == '/' else filepath + '/'
+        inFile = QFile(filepath + filename)
+        text_in = QTextStream(inFile)
+        txt_to_find = self.ui.txtTextToFind.text()
+        if self.regex_text:
+            does_match = re.compile(txt_to_find).match
+        if inFile.open(QIODevice.ReadOnly):
+            while not text_in.atEnd():
+                line = text_in.readLine()
+                if self.regex_text:
+                    if does_match(line):
+                        self.match_count += 1
+                        return True
+                else:
+                    if txt_to_find in line:
+                        self.match_count += 1
+                        return True
+                return False
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    myWindow = Monitor()
+    myWindow.show()
+
+    sys.exit(app.exec_())
